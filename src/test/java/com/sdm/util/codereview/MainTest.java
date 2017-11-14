769735c60c0ab12060d0d81efdd9d4619e800099
Jasbir Minhas <jasbir.s.minhas@gmail.com>
2017-11-14 15:37:06 -0500

    Changed JUnit to version 4.

--- src/test/java/com/sdm/util/codereview/MainTest.java
+++ src/test/java/com/sdm/util/codereview/MainTest.java
@@ -1,10 +1,23 @@
 package com.sdm.util.codereview;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-class MainTest {
-    @org.junit.jupiter.api.Test
-    void main() {
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void main() throws Exception {
+        assertEquals(2, 1 + 2);
     }
 
 }
